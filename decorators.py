81351a33daf777a29d763b08ec009b4996052957
Meghnath Jaishi <meghnathjaishi@gmail.com>
2025-05-23 11:54:16 -0500

    New decorators added and functions modified

--- decorators.py
+++ decorators.py
@@ -16,7 +16,7 @@ def my_decorator(func):
 def say_hello():
     print("Hello 2025!!")
 
-# Here: say_hello() = my_decorator(say_hello)
+# After decorating the function behaves like: say_hello() = my_decorator(say_hello)
 # test 1
 # say_hello()
 
@@ -24,31 +24,33 @@ def say_hello():
 def repeat_decorator(times):
     def decorator(func):
         def wrapper(*args, **kwargs): # *args: positional, **kwargs: key-values arguments
+            print(f"Calling function {func.__name__} {times} times with positional arguments {args} and keyword arguments {kwargs}")
             for _ in range(times):
                 func(*args, **kwargs)
+            print(f"Function {func.__name__} successfully called {times} times")
         return wrapper
     return decorator
 
-@repeat_decorator(times=3)
-def say_foobar():
-    print("foo : bar!!")
+@repeat_decorator(times=4)
+def repeat_utterances(d, name):
+    print(f"Okay, {name}, Let's meet again in {d} days!")
 
-# say_foobar()
+# repeat_utterances(15, name="Maria")
 
 # logging decorator
 def log_decorator(func):
     def wrapper(*args, **kwargs):
         print(f"Calling function {func.__name__} with arguments {args} and {kwargs}")
         result = func(*args, **kwargs)
-        print(f"Function {func.__name__} returned {result}")
+        print(f"Function {func.__name__} returned: {result}")
         # return result
     return wrapper
 
 @log_decorator
-def add(a,b):
-    return a+b
+def population(a,b, country):
+    return f"The human poputation of {country} is roughly {a+b} billion"
 
-# add(2,3)
+# population(0.7, 0.7, country="India")
 
 # Authentication decorator
 def require_auth(func):
@@ -62,10 +64,10 @@ def require_auth(func):
 
 @require_auth
 def view_dashboard(*args, **kwargs):
-    print("Welcome to the admin dashboard!")
+    print(f"Welcome to the admin dashboard of {args[0]}!!")
 
-# view_dashboard(user="guest") # Unauthorized
-# view_dashboard(user="admin") # Welcome to the admin dashboard
+# view_dashboard("TopWorld", user="guest") # Unauthorized
+# view_dashboard("TopWorld", user="admin") # Welcome to the admin dashboard
 
 import time
 # Timing decorator
@@ -74,25 +76,24 @@ def timing_decorator(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Function {func.__name__} took {end_time - start_time:.2f} seconds")
-        return result
+        print(f"Function {func.__name__} took: {end_time - start_time:.2f} seconds")
+        print(f"Finally I am {result}")
     return wrapper
 
 @timing_decorator
 def slow_function():
-    time.sleep(5)
-    print("done!")
+    time.sleep(3)
+    return "done!"
 
 # slow_function()
-# Python built in decorators: @staticmethod, @classmethod, @property
+# Some python built in decorators: @staticmethod, @classmethod, @property
 
-# Both positional and keyword argument
+# Some more decorators with both args and kwargs
 def alt_decorator(func):
     def wrapper(*args, **kwargs): # captures any arguments
         print(f"Arguments passed to the function: args={args}, kwargs={kwargs}")
         result = func(*args, **kwargs) # passes args to original functions
-        print(f"Result of the function {result}")
-        return result
+        print(f"Result of the function: {result}")
     return wrapper
 
 @alt_decorator
@@ -105,14 +106,41 @@ def org_function(a, b, c=8):
 def greet_decorator(func):
     def wrapper(*args, **kwargs):
         print(f"Calling {func.__name__} with args={args} and kwargs={kwargs}")
-        result = func(*args, **kwargs)
-        print(f"{func.__name__} returned {result}")
-        return result
+        # result = func(*args, **kwargs)
+        # print(f"{func.__name__} returned {result}")
+        print(func(*args, **kwargs)) # Call the original function
     return wrapper
 
 @greet_decorator
 def greet(name, greeting="Happy days ahead"):
     return f"{greeting}, {name}!"
 
-greet("Alice")
-greet("Bob", greeting="Hello")
+# greet("Alice")
+# greet("Bob", greeting="Hello")
+
+# Somr more examples 
+
+def authinate(func):
+    def wrapper(*args, **kwargs):
+        user = kwargs.get("User")
+        if user == "TVC":
+            print("Permission Denied! Please contact your contract manager")
+            return
+        elif user ==  "FTE":
+            print("Unauthorized! Please check with your mananger")
+            return
+        elif user == "Admin":
+            return func(*args, **kwargs)
+        else:
+            return
+    return wrapper
+
+@authinate
+def authorize(*args, **kwargs):
+    print(f"Welcome to the admin dashboard of {args[0]}!!")
+
+authorize("TopWorld", User="TVC")
+authorize("TopWorld", User="FTE")
+authorize("TopWorld", User="Admin")
+        
+
